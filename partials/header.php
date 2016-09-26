30318ba2da58697e79c19434da05da1e66bddc78
Adam Taylor <adam@edgecreativesolutions.com>
2016-09-26 13:05:03 +0100

    Place header inside a container

--- partials/header.php
+++ partials/header.php
@@ -148,21 +148,25 @@
 	</svg>
 
 	<header class="header">
-		<a href="index.php">
-			<h1 class="site-name">Byte-Sized News</h1>
-		</a>
+		<div class="container header__inner">
+			
+			<a href="index.php">
+				<h1 class="site-name">Byte-Sized News</h1>
+			</a>
 
-		<div class="header__right">
-			<?php if ( 'about.php' !== $view->getTemplate() ): ?>
-				<a class="header__how small" href="about.php">How was this made?</a>
-			<?php endif ?>
-			<a class="social" href="https://www.twitter.com/IAmAdamTaylor">
-				<svg class="social__icon" fill="#ffffff" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#twitter"></use></svg>
-				<span class="sr-only">Follow me on Twitter</span>
-			</a>
-			<a class="social" href="https://www.github.com/ThisProjectFilePath">
-				<svg class="social__icon" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><use xlink:href="#github"></use></svg>
-				<span class="sr-only">View on GitHub</span>
-			</a>
+			<div class="header__right">
+				<?php if ( 'about.php' !== $view->getTemplate() ): ?>
+					<a class="header__how small" href="about.php">How was this made?</a>
+				<?php endif ?>
+				<a class="social" href="https://www.twitter.com/IAmAdamTaylor">
+					<svg class="social__icon" fill="#ffffff" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#twitter"></use></svg>
+					<span class="sr-only">Follow me on Twitter</span>
+				</a>
+				<a class="social" href="https://www.github.com/ThisProjectFilePath">
+					<svg class="social__icon" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><use xlink:href="#github"></use></svg>
+					<span class="sr-only">View on GitHub</span>
+				</a>
+			</div>
+
 		</div>
 	</header>
